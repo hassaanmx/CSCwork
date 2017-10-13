96c69a5b0f61c4571f748e71fa238d64977a8d87
Hassaan <mustaf65@DESKTOP-FGH7OPO.localdomain>
2017-10-12 22:10:22 -0400

    369

---
+++ 369/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_/home/student/repo_mustaf65/built-in.o :=  rm -f /home/student/repo_mustaf65/built-in.o; ar rcs /home/student/repo_mustaf65/built-in.o
---
+++ 369/.interceptor.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/student/repo_mustaf65/interceptor.ko := ld -r -m elf_i386 -T /usr/src/linux-source-2.6.32/scripts/module-common.lds --build-id -o /home/student/repo_mustaf65/interceptor.ko /home/student/repo_mustaf65/interceptor.o /home/student/repo_mustaf65/interceptor.mod.o
